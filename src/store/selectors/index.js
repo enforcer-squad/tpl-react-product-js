0aca7699398af0db9753310a3227d7124d7b6d2e
enforcer.a06 <enforcer.a06@gmail.com>
2023-12-22 19:03:40 +0800

    feat: 增加selector缓存机制

--- src/store/selectors/index.js
+++ src/store/selectors/index.js
@@ -1,11 +1,10 @@
-import { useMemo } from 'react';
 import appModel from '@/store/app';
 import { useUSS } from '@enforcer-squad/uss';
+import memoize from 'fast-memoize';
 
-export const useTodos = () => {
-  const { todos, filter } = useUSS(appModel);
-
-  const ret = useMemo(() => {
+const selector = memoize(
+  (todos, filter) => {
+    console.log('执行了');
     switch (filter) {
       case 'all':
         return todos;
@@ -14,7 +13,19 @@ export const useTodos = () => {
       default:
         throw Error('Error: un supported filter');
     }
-  }, [todos, filter]);
+  },
+  // {
+  //   serializer: ([todos, filter]) => {
+  //     const calTodos = todos.map(item => {
+  //       return { id: item.id, completed: item.completed };
+  //     });
+  //     return JSON.stringify([calTodos, filter]);
+  //   },
+  // },
+);
 
-  return ret;
+export const useTodos = () => {
+  const { todos, filter } = useUSS(appModel);
+
+  return selector(todos, filter);
 };
